32c305b62982e24162a9020b7f2e540bdf66a5e1
DenilGevariya <gevariyadenil0055@gmail.com>
2025-09-28 11:56:02 +0530

    done

---
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import AddNewSessionDialog from './AddNewSessionDialog'
+
+function HistoryList() {
+    const [historyList,setHistoryList]=useState([])
+  return (
+    <div className='mt-10 '>
+        {historyList.length==0?
+        <div className='flex items-center flex-col p-7 justify-center  border-dashed rounded-2xl border-2'>
+            <Image src={'/medical-assistance.png'} alt='image' width={150} height={150} />
+            <h2 className='font-bold text-xl mt-2'>No Recent Consultations</h2>
+            <p >It looks like you haven't consulted with any doctors yet.</p>
+            <AddNewSessionDialog/>
+        </div>
+        :<div>List</div>}
+    </div>
+  )
+}
+
+export default HistoryList
---
+++ app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+import axios from 'axios';
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { doctorAgent } from '../../_components/DoctorAgentCard';
+import { Circle, PhoneCall, PhoneOff } from 'lucide-react';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Vapi from '@vapi-ai/web';
+
+type SessionDetail={
+  id:number,
+  notes:string,
+  sessionId:string,
+  report:JSON,
+  selectedDoctor:doctorAgent,
+  createdOn:string,
+
+}
+
+function VoiceAgent() {
+  const {sessionId}=useParams();
+const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
+  const [sessionDetail,setSessionDetail]=useState<SessionDetail>()
+  const [callStarted,setCallStarted]=useState(false)
+  useEffect(()=>{
+    sessionId&&GetSessionDetails();
+  },[sessionId])
+  const GetSessionDetails= async()=>{
+    const result=await axios.get('/api/session-chat?sessionId='+sessionId);
+    console.log(result.data)
+    setSessionDetail(result.data)
+  }
+
+  const StartCall=()=>{
+    vapi.start(process.env.NEXT_PUBLIC_VAPI_VOICE_ASSISTANT_ID);
+    vapi.on('call-start', () => {console.log('Call started')
+      setCallStarted(true)
+    });
+vapi.on('call-end', () =>{ console.log('Call ended')
+  setCallStarted(false)
+});
+vapi.on('message', (message) => {
+  if (message.type === 'transcript') {
+    console.log(`${message.role}: ${message.transcript}`);
+  }
+});
+  }
+  return (
+    <div className='p-5 bg-secondary border rounded-3xl'>
+      <div className='flex justify-between items-center'>
+        <h2 className='p-1 px-2 border rounded-md flex gap-2 items-center'><Circle className={`h-4 w-4 rounded-full ${callStarted?'bg-green-500':'bg-rd-500'} `}/>{callStarted?'Connected..':'Not Connected'}</h2>
+        <h2 className='font-bold text-xl text-gray-400'>00:00</h2>
+      </div>
+
+{ sessionDetail&& <div className='flex items-center flex-col mt-10'>
+        <Image src={sessionDetail?.selectedDoctor?.image}
+         alt={sessionDetail?.selectedDoctor?.specialist}
+         width={80}
+         height={80}
+         className='h-[100px] w-[100px] object-cover rounded-full'/>
+         <h2
+         className='mt-2 text-lg'
+         >{sessionDetail?.selectedDoctor?.specialist}</h2>
+         <p className='text-sm text-gray-400'>Ai Medical Voice Agent</p>
+         <div className='mt-32'>
+          <h2 className='text-gray-400'>Assistant Message</h2>
+          <h2 className='text-lg'>User Mesage</h2>
+         </div>
+         {!callStarted?<Button className='mt-2' onClick={StartCall}>
+          <PhoneCall/>Start Call
+          </Button>:<Button variant={'destructive'}><PhoneOff/>Disconnect</Button>}
+      </div>}
+    </div>
+  )
+}
+
+export default VoiceAgent
